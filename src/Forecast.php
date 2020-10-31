b2c21205c36d27b4f36b2fc166736960193bccc0
Marc Grau <marcgrau@MacBook-MacBook-Pro-de-Marc.local>
2020-10-31 17:39:12 +0100

    code clean improvements

--- src/Forecast.php
+++ src/Forecast.php
@@ -6,6 +6,10 @@ use GuzzleHttp\Client;
 
 class Forecast
 {
+
+    private const META_QUERY = "https://www.metaweather.com/api/location/search/?query=";
+    private const META_LOCATION = "https://www.metaweather.com/api/location/";
+
     public function predict(string &$city, \DateTime $datetime = null, bool $wind = false): string
     {
         // When date is not provided we look for the current prediction
@@ -14,34 +18,33 @@ class Forecast
         }
 
         // If there are predictions
-        if ($datetime < new \DateTime("+6 days 00:00:00")) {
-
-
-            // Create a Guzzle Http Client
-            $client = new Client();
-
-            // Find the id of the city on metawheather
-            $woeid = json_decode($client->get("https://www.metaweather.com/api/location/search/?query=$city")->getBody()->getContents(),
-                true)[0]['woeid'];
-            $city = $woeid;
-
-            // Find the predictions for the city
-            $results = json_decode($client->get("https://www.metaweather.com/api/location/$woeid")->getBody()->getContents(),
-                true)['consolidated_weather'];
-            foreach ($results as $result) {
-
-                // When the date is the expected
-                if ($result["applicable_date"] == $datetime->format('Y-m-d')) {
-                    // If we have to return the wind information
-                    if ($wind) {
-                        return $result['wind_speed'];
-                    } else {
-                        return $result['weather_state_name'];
-                    }
-                }
-            }
-        } else {
+        $valid_date = $datetime < new \DateTime("+6 days 00:00:00");
+        if (!$valid_date) {
             return "";
+        }
+
+        // Create a Guzzle Http Client
+        $client = new Client();
+
+        // Find the id of the city on metawheather
+        $woeid = json_decode($client->get(self::META_QUERY . $city)->getBody()->getContents(),
+            true)[0]['woeid'];
+        $city = $woeid;
+
+        // Find the predictions for the city
+        $results = json_decode($client->get(self::META_LOCATION . $woeid)->getBody()->getContents(),
+            true)['consolidated_weather'];
+        foreach ($results as $result) {
+
+            // When the date is the expected
+            if ($result["applicable_date"] == $datetime->format('Y-m-d')) {
+                // If we have to return the wind information
+                if ($wind) {
+                    return $result['wind_speed'];
+                }
+                    
+                return $result['weather_state_name'];
+            }
         }
     }
 }
